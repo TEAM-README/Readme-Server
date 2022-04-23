63e2a3a624560d2a52cbfc607b3591ae7081d04d
junguksim <simju9397@gmail.com>
2022-04-23 18:09:37 +0900

    [FEAT] Add response interceptor to format api response

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,11 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return data "Hello World!"', () => {
+      expect(appController.getHello()).toStrictEqual({
+        message: 'Get Hello 성공',
+        data: 'Hello World!',
+      });
     });
   });
 });
---
+++ src/response.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<S, T> {
+  message: S;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<S, T>
+  implements NestInterceptor<T, Response<S, T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<S, T>> {
+    const status = context.switchToHttp().getResponse().statusCode;
+    return next
+      .handle()
+      .pipe(map(({ message, data }) => ({ status, message, data })));
+  }
+}
