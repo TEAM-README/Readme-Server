7854cc331cbb673a0098635768ade10bb66d47f2
junguksim <simju9397@gmail.com>
2022-05-06 18:27:06 +0900

    [FEAT] DB Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { ResponseInterceptor } from './response.interceptor';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [
     AppService,
