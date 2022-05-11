8a22509b0c647a65ed17623e8585a953dbf8e870
junguksim <simju9397@gmail.com>
2022-05-11 21:26:30 +0900

    [FEAT] User resource 추가

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Feed } from '../../feed/entities/feed.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  uid: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  refresh_token: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Column({ default: false })
+  is_deleted: boolean;
+
+  @OneToMany(() => Feed, (feed) => feed.user_id)
+  feeds: Feed[];
+}
