e7ba2184bcb120cb0bc62914de2417cd1df8c77e
eunji <eunjikim8784@gmail.com>
2022-05-15 12:29:27 +0900

    [DELETE] delete useless module imports

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,8 +11,6 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
-import { ApiResponse } from 'src/types/global';
 
 @Controller('user')
 export class UserController {
