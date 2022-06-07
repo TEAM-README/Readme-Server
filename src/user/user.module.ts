250f33a700396ea469ec5eacb3dcb0328a9f9b65
eunji <eunjikim8784@gmail.com>
2022-06-07 20:53:32 +0900

    [FEAT] add exports user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -16,5 +16,6 @@ import { AuthModule } from 'src/auth/auth.module';
   ],
   controllers: [UserController],
   providers: [UserService, AuthService],
+  exports: [UserService],
 })
 export class UserModule {}
