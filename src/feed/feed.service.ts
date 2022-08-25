639426f0c838bd6c303ff46ee96723f1a9bfe2cf
Hyosik Philip Joo <lux1937@gmail.com>
2022-08-25 16:26:20 +0900

    [FEAT] Exception handling - no ISBN, author case

--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -25,10 +25,29 @@ export class FeedService {
   ): Promise<ApiResponse<{ id: number; createdAt: Date }>> {
     try {
       if (!createFeedDto.book.isbn) {
-        const fakeISBN = Math.floor(Math.random() * 10000000000).toString() + 'F';
-        createFeedDto.book.isbn = fakeISBN;
+        // 해당 도서의 ISBN이 존재하지 않는 경우
+        const fakeISBN =
+          Math.floor(Math.random() * 10000000000).toString() + 'F';
+        if (!createFeedDto.book.author) {
+          // 해당 도서의 저자 정보도 존재하지 않는 경우 : 중복 확인 불가. 새로운 책 생성
+          createFeedDto.book.isbn = fakeISBN;
+          await this.booksRepository.save(createFeedDto.book);
+        } else {
+          // 해당 도서의 저자 정보는 존재하는 경우 : 제목, 저자로 중복 확인
+          const existingBook = await this.booksRepository.findOneBy({
+            title: createFeedDto.book.title,
+            author: createFeedDto.book.author,
+          });
+          if (existingBook) {
+            // 기존 등록된 책 존재 : 기존 등록된 책을 createFeedDto에 할당
+            createFeedDto.book = existingBook;
+          } else {
+            // 책 신규 등록 : 랜덤 fakeISBN을 활용해 book Repository에 새로운 책 생성
+            createFeedDto.book.isbn = fakeISBN;
+            await this.booksRepository.save(createFeedDto.book);
+          }
+        }
       }
-      await this.booksRepository.save(createFeedDto.book);
 
       const { id, createdAt } = await this.feedsRepository.save({
         ...createFeedDto,
