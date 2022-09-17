6772eaf4ec952cca0dbc381d2b044807d50c5e12
Hyosik Philip Joo <lux1937@gmail.com>
2022-09-17 15:31:03 +0900

    [CHORE] Receive DataSource options from env

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,16 +1,15 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export const connectionSource = new DataSource({
   type: 'postgres',
-  host: 'readme-db.cnhsfzeymdfu.ap-northeast-2.rds.amazonaws.com',
-  port: 5432,
-  username: 'readme',
-  password: 'flemaltjqj4$',
-  database: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/**/*.entity.{js,ts}'],
   namingStrategy: new SnakeNamingStrategy(),
-  synchronize: false,
-  migrationsTableName: 'migrations',
+  synchronize: process.env.NODE_ENV === 'dev', // dev이면 synchronize = true, prod이면 synchronize = false
   migrations: ['dist/migrations/*{.ts,.js}'],
 });
